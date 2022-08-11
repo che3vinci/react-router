28064c6abe0d3f1ff488527674c565fd30fd1ccd
jinbo.wang <che3vinci@gmail.com>
2022-08-11 14:48:31 +0800

    chore: chore

--- packages/react-router/__tests__/useNavigate-test.tsx
+++ packages/react-router/__tests__/useNavigate-test.tsx
@@ -9,6 +9,42 @@ import {
 } from "react-router";
 
 describe("useNavigate", () => {
+  it("useStater", () => {
+    function Home() {
+      const [s1, setS1] = React.useState(1);
+      const [s2, setS2] = React.useState(2);
+      function handleClick() {
+        setS1(s1 + 1);
+        setS2(s2 + 2);
+      }
+
+      return (
+        <div>
+          <h1>s1={s1}</h1>
+          <h1>s2={s2}</h1>
+          <button onClick={handleClick}>click me</button>
+        </div>
+      );
+    }
+
+    let renderer: TestRenderer.ReactTestRenderer;
+    TestRenderer.act(() => {
+      renderer = TestRenderer.create(<Home />);
+    });
+
+    let button = renderer.root.findByType("button");
+
+    TestRenderer.act(() => {
+      button.props.onClick();
+    });
+
+    console.log(renderer.toJSON());
+  });
+  
+  
+  
+  
+
   it("transitions to the new location", () => {
     function Home() {
       let navigate = useNavigate();
